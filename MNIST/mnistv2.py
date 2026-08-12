34c47a2655ca4882290e4a7da6d2042f55188774
Hitman45 <dreamchasercoder@gmail.com>
2026-08-12 17:44:12 +0545

    Finish exercises 1 and 2.

--- MNIST/mnistv2.py
+++ MNIST/mnistv2.py
@@ -6,6 +6,7 @@ mnist = fetch_openml('mnist_784', as_frame=False)
 
 X, y = mnist['data'], mnist['target']
 
+
 # %%
 import matplotlib.pyplot as plt
 def plot_num(some_digit):
@@ -16,6 +17,7 @@ image = X[0]
 plot_num(image)
 plt.show()
 
+
 # %%
 from sklearn.preprocessing import StandardScaler
 
@@ -24,11 +26,13 @@ X_train, X_test, y_train, y_test = X[:60000], X[60000:], y[:60000], y[60000:]
 scaler = StandardScaler()
 X_train_scaled = scaler.fit_transform(X_train, y_train)
 
+
 # %%
 from sklearn.neighbors import KNeighborsClassifier
 
 kn_clf = KNeighborsClassifier()
 kn_clf.fit(X_train_scaled, y_train)
+
 
 # %%
 
@@ -46,3 +50,49 @@ print(num)
 # %%
 plot_num(X_test[34])
 
+
+# %%
+# acc = cross_val_score(kn_clf, X_train, y_train, cv=3, scoring="accuracy", n_jobs=-1)
+# print(acc)
+
+
+# %%
+param_grid = {
+        'n_neighbors': [3, 4, 5] ,
+        'weights': ['uniform', 'distance'],
+    } 
+
+
+# %%
+from sklearn.model_selection import GridSearchCV
+
+grid_search = GridSearchCV(kn_clf, param_grid= param_grid, cv=3, scoring='accuracy')
+grid_search.fit(X_train_scaled, y_train)
+
+print("Best parameters found:", grid_search.best_params_)
+print("Best cross-validation:", grid_search.best_score_)
+
+
+# %%
+
+grid_search = GridSearchCV(kn_clf, param_grid= param_grid, cv=3, scoring='accuracy')
+grid_search.fit(X_train, y_train)
+
+print("Best parameters found:", grid_search.best_params_)
+print("Best cross-validation:", grid_search.best_score_)
+
+
+# %%
+from scipy.ndimage import shift
+
+def shift_image(image: np.array):
+  image = image.reshape(28, 28)
+  return (  (shift(image, (0,0))).reshape(784),
+            (shift(image, (-1,0))).reshape(784),
+            (shift(image, (0,1))).reshape(784),
+            (shift(image, (1,0))).reshape(784),
+            (shift(image, (1,0))).reshape(784)
+          )
+
+# %%
+X_train_backup = np.array(list(map(shift_image, X_train)))
